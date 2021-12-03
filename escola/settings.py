0d095a727c52a18659c785adaa127e08bbd49b7a
Alexandre Sampaio <thndrmaster@gmail.com>
2021-12-03 15:46:27 -0300

    Criando Estrutura Base com MongoDB

--- escola/settings.py
+++ escola/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'material',
+    'material.admin',
+
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -85,9 +88,7 @@ WSGI_APPLICATION = 'escola.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'db-name',
-        'USERNAME': 'mongoAdmin',
-        'PASSWORD': 'admin'
+        'NAME': 'descola',
     }
 }
 
