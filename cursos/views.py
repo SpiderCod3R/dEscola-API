4dfd1fa99650bbe34b0adb14e34338506797eb23
Alexandre Sampaio <thndrmaster@gmail.com>
2021-12-06 12:20:46 -0300

    Adicionando Rotas e sobreescrevendo metodos

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+
 
 
 class CursosAPIView(generics.ListCreateAPIView):
@@ -17,7 +20,19 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_id'):
+            return get_object_or_404(self.get_queryset(),
+                                     curso_id=self.kwargs.get('curso_id'),
+                                     pk= self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.queryset(), pk=self.kwargs)
